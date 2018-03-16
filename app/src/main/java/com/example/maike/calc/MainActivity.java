7dd7b8ea8659105ffc4242c54e7a9080245ea8cd
DESKTOP-2N3KM0P\WackyMonkey <maikel-emmanuel@hotmail.nl>
2018-03-16 17:04:48 +0100

    finished caculator

--- app/src/main/java/com/example/maike/calc/MainActivity.java
+++ app/src/main/java/com/example/maike/calc/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.maike.calc;
 
 import android.app.Activity;
+import android.media.VolumeShaper;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,8 +10,17 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    String runningNumber = "";
     TextView resultView;
+
+    public enum Operation{
+        ADD, SUBTRACT, DIVIDE, MULTIPLY,EQUAL
+    }
+
+    String runningNumber = "";
+    String leftValueStr = "";
+    String rightValueStr = "";
+    Operation currentOperation;
+    int result = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -114,47 +124,89 @@ public class MainActivity extends Activity {
         add_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                processOperation(Operation.ADD);
             }
         });
 
         substract_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                processOperation(Operation.SUBTRACT);
             }
         });
 
         multiply_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                processOperation(Operation.MULTIPLY);
             }
         });
 
         divide_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                processOperation(Operation.DIVIDE);
             }
         });
 
         clear_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                leftValueStr = "";
+                rightValueStr = "";
+                result = 0;
+                runningNumber = "";
+                currentOperation = null;
+                resultView.setText("");
             }
         });
 
         equal_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                processOperation(Operation.EQUAL);
             }
         });
 
 
     }
+
+    void processOperation(Operation operation){
+        if (currentOperation != null){
+
+            if (runningNumber != ""){
+                rightValueStr = runningNumber;
+                runningNumber = "";
+
+                switch (currentOperation){
+                    case ADD:
+                        result = Integer.parseInt(leftValueStr) + Integer.parseInt(rightValueStr);
+                        break;
+                    case SUBTRACT:
+                        result = Integer.parseInt(leftValueStr) - Integer.parseInt(rightValueStr);
+                        break;
+                    case DIVIDE:
+                        result = Integer.parseInt(leftValueStr) / Integer.parseInt(rightValueStr);
+                        break;
+                    case MULTIPLY:
+                        result = Integer.parseInt(leftValueStr) * Integer.parseInt(rightValueStr);
+                        break;
+                }
+
+                leftValueStr = String.valueOf(result);
+                resultView.setText(leftValueStr);
+            }
+
+
+        }else {
+            leftValueStr = runningNumber;
+            runningNumber = "";
+
+        }
+        currentOperation = operation;
+    }
+
+
     void numberPressed(int number){
         runningNumber += String.valueOf(number);
         resultView.setText(runningNumber);
